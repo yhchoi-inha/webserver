ebaf5e86f98e0d7c37c7dbedecc1fff2ad187f7a
Yohan <yohan0312@inha.edu>
2023-08-26 14:50:13 +0900

    add : webserver setting

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express'); // 익스프레스 모듈(익스프레스 내부에 http 모듈 내장)
+const path = require('path');
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => { // req(요청에 대한 정보), res(응답에 관한 정보)
+    // res.send('testing');
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '빈 포트 대기');
+})
